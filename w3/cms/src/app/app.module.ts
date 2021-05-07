8401dbb3f66d137613ff981ed7dcda2889719c01
splucena <sumilangplucena@yahoo.com.ph>
2021-05-07 21:30:55 +0800

    started week 3 assignment

---
+++ w3/cms/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+import {ContactItemComponent} from './contacts/contact-item/contact-item.component';
+import { DocumentComponent } from './documents/document.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    HeaderComponent,
+    ContactItemComponent,
+    DocumentComponent,
+    DocumentListComponent,
+    DocumentDetailComponent,
+    DocumentItemComponent,
+    MessagesComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ w3/cms/src/app/documents/document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-documents',
+  templateUrl: './document.component.html',
+  styleUrls: ['./document.component.css']
+})
+export class DocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
