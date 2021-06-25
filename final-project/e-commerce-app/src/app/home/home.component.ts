dc81cdf202e24a2306155a49c57a197242b0b34d
splucena <sumilangplucena@yahoo.com.ph>
2021-06-26 00:41:02 +0800

    added home content

--- final-project/e-commerce-app/src/app/home/home.component.ts
+++ final-project/e-commerce-app/src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from '../products/product.model';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  products: Product[] = [];
+  private subscription: Subscription;
+  searchTerm: string = '';
 
-  constructor() { }
+  constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.subscription = this.productService.productChangeEvent.subscribe(
+      (products: Product[]) => {
+        this.products = products;
+      }
+    );
   }
 
+  search(value: string) {
+    this.searchTerm = value;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
